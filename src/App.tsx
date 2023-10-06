29be8c5fcb4549e13f120a87c774c83766f9d605
Lúcio Henrique Alves Silveira <lucions96@gmail.com>
2023-10-06 16:38:42 -0300

    adiciona dependencias

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+
+function App(): JSX.Element {
+  return <View />;
+}
+
+export default App;
